a16076c9c292680178f295c7077557d3e337faf0
Asyraf B.A.R <work@officialasyraf.info>
2026-08-27 10:58:52 +0800

    feat: echo user commands and exit on 'bye'
    Read each line of input and echo it back framed by separators.
    Terminate the app when the user types 'bye'. Tagged as Level-1 increment.

--- src/main/java/Maple.java
+++ src/main/java/Maple.java
@@ -1,7 +1,15 @@
+/**
+ * Entry point for the Maple chatbot.
+ */
 public class Maple {
     public static void main(String[] args) {
         Ui ui = new Ui();
         ui.showWelcome();
+        String command = ui.readCommand();
+        while (!command.equals("bye")) {
+            ui.echo(command);
+            command = ui.readCommand();
+        }
         ui.showExit();
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Handles Maple's user-facing input and output.
  */
@@ -9,16 +11,41 @@ public class Ui {
             + "/_/  /_/\\_,_/ .__/_/\\__/ \n"
             + "           /_/           ";
 
+    private final Scanner scanner = new Scanner(System.in);
+
     public void showWelcome() {
         System.out.println(SEPARATOR);
         System.out.println(BANNER);
         System.out.println("Hello! I'm Maple.");
         System.out.println("What can I do for you?");
         System.out.println(SEPARATOR);
+        System.out.println();
+    }
+
+    /**
+     * Reads the next line of user input.
+     *
+     * @return the command entered by the user
+     */
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    /**
+     * Echoes the given command back to the user, framed by separators.
+     *
+     * @param command the command to echo
+     */
+    public void echo(String command) {
+        System.out.println(SEPARATOR);
+        System.out.println("     " + command);
+        System.out.println(SEPARATOR);
+        System.out.println();
     }
 
     public void showExit() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEPARATOR);
+        System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(SEPARATOR);
     }
 }
